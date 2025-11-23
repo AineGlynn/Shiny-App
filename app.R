8a4ef334c3c15817c4fcce4f6660ea6ead27ce69
Maeve Samali <maeve.samali.413@gmail.com>
2025-11-23 13:57:36 +0000

    Addition of an inital survival plot to be improved later

--- app.R
+++ app.R
@@ -3,27 +3,10 @@ library(tidyverse)
 library(readr)
 library(ggplot2)
 library(plotly)
+library(survival)
+library(survminer)
 
 dig.df <- read_csv("DIG.csv")
-
-
-#ui <- fluidPage(
-#  titlePanel("DIG Data Explorer"),
-#  sidebarLayout(
-#    sidebarPanel(
-#      checkboxGroupInput(inputId = "Treatment", label = "Treatment Group" , choices = c("Treatment", "Placebo"), selected = NULL)
-#    ),
-#    mainPanel(
-      
-#    )
-#  )
-#)
-
-#server <- function(input, output) {
-  
-#}
-
-#shinyApp(ui, server)
 
 dig.df <- dig.df %>%
   select(ID, TRTMT, AGE, SEX, BMI, KLEVEL, CREAT, DIABP, SYSBP, HYPERTEN, CVD, WHF, DIG, HOSP, HOSPDAYS, DEATH, DEATHDAY) %>%
@@ -31,22 +14,26 @@ dig.df <- dig.df %>%
                       levels = c(1, 2), 
                       labels = c('Male', 'Female')), 
          TRTMT = factor(TRTMT,
-                        levels = c(0, 1),
-                        labels = c('Placebo', 'Treatment')))
+                        levels = c(1,0),
+                        labels = c('Treatment', 'Placebo')),
+         Death_Month = round(DEATHDAY/30)) 
+
 
 ui <- fluidPage(
   titlePanel("DIG Data Exploration"),
   sidebarLayout(
     sidebarPanel(
+      #checkboxGroupInput(inputId = "TRTMT", label = "Treatment Group" , choices = c("Treatment", "Placebo"), selected = NULL),
       selectInput(inputId = "SEX", label = "Select Gender:", choices = c("Male", "Female"), multiple = TRUE, selected = c("Male", "Female")),
       sliderInput("BMI", "BMI Range:", min = 14, max = 65, value = c(20, 50)),
       #changing the age so that a range can be selected
-      sliderInput("AGE", "Participant Age:", min = 20, max = 95, value = c(30, 60))
-      
+      sliderInput("AGE", "Participant Age:", min = 20, max = 95, value = c(30, 60)),
+      sliderInput("Death_Month", "Follow up time in months:", min = 0, max = 60, value = c(0,10), animate = TRUE)
     ),
     mainPanel(
       plotOutput("plot1", click = "plot1_click"),
       plotOutput("plot2", click = "plot2_click"),
+      plotOutput("plot3"),
       dataTableOutput("table1"),
       dataTableOutput("table2")
     )
@@ -59,8 +46,10 @@ server <- function(input, output) {
   DIG_sub <- reactive({
     dig.df %>%
       filter(SEX %in% input$SEX) %>%
-     filter(AGE >= input$AGE[1] & AGE <= input$AGE[2]) %>%
-    filter(BMI >= input$BMI[1] & BMI <= input$BMI[2])
+      filter(AGE >= input$AGE[1] & AGE <= input$AGE[2]) %>%
+      filter(BMI >= input$BMI[1] & BMI <= input$BMI[2]) %>%
+      #filter(TRTMT %in% input$TRTMT)
+      filter(Death_Month >= input$Death_Month[1] & Death_Month <= input$Death_Month[2])
   })
   
   output$plot1 <- renderPlot({ 
@@ -69,6 +58,7 @@ server <- function(input, output) {
       theme_minimal() 
   })
  
+  #static plot is there a way to improve it?
   output$plot2 <- renderPlot({ 
     ggplot(DIG_sub(), aes(x = TRTMT)) +
       geom_bar(colour = 'black', fill = c('darkorchid', 'darkblue'), alpha = 0.6, width = 0.4) +
@@ -77,6 +67,17 @@ server <- function(input, output) {
            y = 'Total') +
       theme_minimal()
   })
+  #survival function
+  surv_func <- reactive({
+    survfit(as.formula(paste("Surv(Death_Month, DEATH)~", paste(1))), data = DIG_sub())})
+  
+  #still working on it
+  output$plot3 <- renderPlot({
+    #digfit = survfit(as.formula(paste("Surv(Death_Month, DEATH)~", paste(1))), data = DIG_sub())
+    #ggsurvplot(surv_func(), data = DIG_sub())
+    plot(surv_func()) # need to improve the plot
+  })
+  
 #clicks:
   output$click_info1 <- renderPrint({
     input$plot1_click
@@ -84,7 +85,7 @@ server <- function(input, output) {
   output$click_info2 <- renderPrint({
     input$plot2_click
   })  
-   
+  
   # Tables
   output$table1 <- renderDataTable({req(input$plot1_click)
     nearPoints(DIG_sub(), input$plot1_click)})
